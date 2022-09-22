e87bb6d2b5bb30614f68e86f6546bc7d26126293
yadav <yadavrajsahil@gmail.com>
2022-09-22 20:47:22 +0530

    new templatet added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Message>
+          Hello,there i am live!
+        </Message>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const Message=styled.div`
+    color: #ffffff;
+    font-size: 30px;
+    font-weight: bold;
+    font-family: monospace;
+`
